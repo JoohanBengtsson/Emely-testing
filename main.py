c2c6a6ee19a120a51c0a31bc8ace4b2bef145d60
JoohanBengtsson <62339966+JoohanBengtsson@users.noreply.github.com>
2021-06-14 14:49:37 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+
+URL = "http://localhost:8080/inference"
+
+feelings = [
+    "happy", "sad", "crazy", "lazy", "funny", "dead"
+]
+
+pronouns = [
+    "I", "you", "me", "he", "she", "we", "they"
+]
+
+nounPersons = [
+    "thief", "police", "singer"
+]
+
+conversation = ["Hi, I am Emely", "Hello, my name is Johan", "Hi Johan, what is your favorite food?", 'Definitely Tacos']
+
+
+if __name__ == '__main__':
+    for i in range(len(conversation)):
+        jsonText = conversation[0]
+        for sentenceIndex in range(1, len(conversation)):
+            jsonText = jsonText + '\n' + conversation[sentenceIndex]
+        jsonObj = {
+            "accept": "application/json",
+            "Content-Type": "application/json",
+            "text": jsonText
+        }
+        r = requests.post(URL, json=jsonObj)
+        resp = r.json()['text']
+        print(resp)
+
+
