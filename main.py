59c35823eee7d0506ea85925f7207aaeaf12c10a
HaraldOsterlingAnimals <harald@animals.ai>
2021-06-15 13:55:06 +0200

    Added blenderbot for conversation

--- main.py
+++ main.py
@@ -1,33 +1,13 @@
 import requests
 import os
 import random
+from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration, BlenderbotConfig
 
-
-URL = "http://localhost:8080/inference"
-
-feelings = [
-    "happy", "sad", "crazy", "lazy", "funny", "dead"
-]
-
-pronouns = [
-    "I", "you", "me", "he", "she", "we", "they"
-]
-
-nounPersons = [
-    "thief", "police", "singer"
-]
-
-#rescueScentences = ["Nice to meet you.",
-#                    "Where are you from?",
-#                    "What do you do?",
-#                    "What do you like to do in your free time?",
-#                    "Do you have Facebook?",
-#                    "What do you think?"]
-
-convarray = ["Hey","Hello! How are you doing today? I just got back from walking my dog, how about you?"] # Array with the conversation.
-#conversation = ["Hi, I am Emely", "Hello, my name is Johan", "Hi Johan, what is your favorite food?", 'Definitely Tacos']
+isBlenderbot = True # True: Emely talks to blenderbot, False: Emely talks to self
+convarray = ["Hey","Hey"] # Array with the conversation.
 
 def array2string(convarray):
+    # Converts the conversation array to a string separated by newline
     convstring = ' '.join([str(elem) + '\n' for elem in convarray])
     convstring = convstring[:len(convstring)-1]
     return convstring
@@ -40,31 +20,64 @@ def add2conversation(convarray, resp):
     else:
         convarray.pop(0)
 
-def getEmelyResponse(convarray):
-    jsonObj = {
-        "accept": "application/json",
-        "Content-Type": "application/json",
-        "text": array2string(convarray)
-    }
-    r = requests.post(URL, json=jsonObj)
-    resp = r.json()['text']
-    return resp
+class blenderbot:
+    def __init__(self):
+        self.name = 'facebook/blenderbot-400M-distill'
+        self.model = BlenderbotForConditionalGeneration.from_pretrained(self.name)
+        self.tokenizer = BlenderbotTokenizer.from_pretrained(self.name)
+
+    def getResponse(self, convarray):
+        convstring = self.__array2blenderstring(convarray)
+        inputs = self.tokenizer([convstring], return_tensors='pt')
+        reply_ids = self.model.generate(**inputs)
+        resp = self.tokenizer.batch_decode(reply_ids, skip_special_tokens=True)[0]
+        return resp
+
+    def __array2blenderstring(self, convarray):
+        convstring = ' '.join([str(elem) + '</s> <s>' for elem in convarray])
+        convstring = convstring[:len(convstring) - 8]
+        return convstring
+
+class emely:
+    def __init__(self):
+        self.URL = "http://localhost:8080/inference"
+
+    def getResponse(self, convarray):
+        # Inputs the conversation array and outputs a response from Emely
+        jsonObj = {
+            "accept": "application/json",
+            "Content-Type": "application/json",
+            "text": array2string(convarray)
+        }
+        r = requests.post(self.URL, json=jsonObj)
+        resp = r.json()['text']
+        return resp
 
 if __name__ == '__main__':
     # If you want to start the chatbot
     #os.system("docker run -p 8080:8080 emely-interview")
 
+    model_emely = emely()
+
+    if isBlenderbot:
+        model_blenderbot = blenderbot()
+
     # Loop a conversation
-    for i in range(20):
+    for i in range(10):
+
         # Get response from the Emely model
-        resp = getResponse(convarray)
+        resp = model_emely.getResponse(convarray)
+        print("Emely: ", resp)
 
-        # Check if response repeats itself. Here it uses a rescue scentence if it does.
-        if convarray[len(convarray)-2] == resp:
-            resp = rescueScentences[random.randint(0, len(rescueScentences)-1)]
+        # Get next response.
+        if isBlenderbot:
+            convarray.append(resp)
+            resp = model_blenderbot.getResponse(convarray[-3:])
+            convarray.append(resp)
+            print("Human: ", resp)
+        else:
+            print("Emely 2: ", resp)
+            resp = add2conversation(convarray,resp)
 
-        # Add response to the conversation
-        add2conversation(convarray, resp)
-
-    # Print the conversation
-    print(array2string(convarray))
+    # Save the entire conversation
+    convstring = array2string(convarray)
--- testAPI.py
+++ testAPI.py
@@ -1,23 +1,20 @@
-import json
-import requests
+from transformers import BlenderbotTokenizer, BlenderbotForConditionalGeneration
+mname = 'facebook/blenderbot-400M-distill'
+model = BlenderbotForConditionalGeneration.from_pretrained(mname)
+tokenizer = BlenderbotTokenizer.from_pretrained(mname)
+UTTERANCE = "My friends are cool but they eat too many carbs."
+print("Human: ", UTTERANCE)
+inputs = tokenizer([UTTERANCE], return_tensors='pt')
+reply_ids = model.generate(**inputs)
+print("Bot: ", tokenizer.batch_decode(reply_ids, skip_special_tokens=True)[0])
 
-API_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill"
-headers = {"Authorization": f"Bearer {API_TOKEN}"}
-
-def query(payload):
-	data = json.dumps(payload)
-	response = requests.request("POST", API_URL, headers=headers, data=data)
-	return json.loads(response.content.decode("utf-8"))
-
-data = query(
-    {
-        "inputs": {
-            "past_user_inputs": ["Which movie is the best ?"],
-            "generated_responses": ["It's Die Hard for sure."],
-            "text": "Can you explain why ?",
-        },
-    }
+#REPLY = "I'm not sure"
+#print("Human: ", REPLY)
+NEXT_UTTERANCE = (
+"My friends are cool but they eat too many carbs.</s> <s>That's unfortunate. "
+"Are they trying to lose weight or are they just trying to be healthier?</s> "
+"<s> I'm not sure."
 )
-
-if __name__ == '__main__':
-    print(data)
+inputs = tokenizer([NEXT_UTTERANCE], return_tensors='pt')
+next_reply_ids = model.generate(**inputs)
+print("Bot: ", tokenizer.batch_decode(next_reply_ids, skip_special_tokens=True)[0])
